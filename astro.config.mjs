6234dd48620a5865f6760c5199c2bbb7cecb5298
Egua Code <eguacode@gmail.com>
2026-06-10 23:21:21 -0300

    chore: remove www da url do site principal

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.comunidadeignicao.com.br',
+  site: 'https://comunidadeignicao.com.br',
   vite: {
     plugins: [tailwindcss()]
   },
